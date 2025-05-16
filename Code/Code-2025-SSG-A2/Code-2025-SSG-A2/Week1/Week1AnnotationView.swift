ae0730ce624b2dbf0777eb6648e35f603eafb1fc
nakisara01 <nakisara01@ajou.ac.kr>
2025-05-16 14:16:16 +0900

    Fix submodule issue by flattening folder

---
+++ Code/Code-2025-SSG-A2/Code-2025-SSG-A2/Week1/Week1AnnotationView.swift
@@ -0,0 +1,18 @@
+//
+//  Week1AnnotationView.swift
+//  Code-2025-SSG-A2
+//
+//  Created by 나현흠 on 5/15/25.
+//
+
+import SwiftUI
+
+struct Week1AnnotationView: View {
+    var body: some View {
+        Text("Joid Page")
+    }
+}
+
+#Preview {
+    Week1AnnotationView()
+}

36c5f36b15b22679b388e927ee43b72be4885de2
nakisara01 <nakisara01@ajou.ac.kr>
2025-05-16 14:16:16 +0900

    Fix submodule issue by flattening folder

---
+++ Code/Code-2025-SSG-A2/Code-2025-SSG-A2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Code-2025-SSG-A2
+//
+//  Created by 나현흠 on 5/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

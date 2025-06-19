391eb794d834a93dd4fa22e887f325329657ed3b
UnkyungJo <pos10022@unist.ac.kr>
2025-06-19 14:47:22 +0900

    추가

--- Code/Code-2025-SSG-A2/Code-2025-SSG-A2/Studys/Navigation/NavNavView.swift
+++ Code/Code-2025-SSG-A2/Code-2025-SSG-A2/Studys/Navigation/NavNavView.swift
@@ -4,7 +4,10 @@ struct NavNavView: View {
     var body: some View {
         NavigationStack{
             List{
+                Text("Tether")
                 NavigationLink("TetherNavigationCompareView", destination: TetherNavigationCompareView())
+                Text("Gyeong")
+                NavigationLink("GyeongTabviewExampleView", destination: GyeongTabviewExampleView())
             }
             .navigationTitle("Navigation")
         }

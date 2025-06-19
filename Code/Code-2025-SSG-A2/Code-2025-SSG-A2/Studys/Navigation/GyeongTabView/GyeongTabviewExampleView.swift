fd88d7b7d6f88d3afbf8b053a8670532ec0e8827
UnkyungJo <pos10022@unist.ac.kr>
2025-06-19 14:47:22 +0900

    추가

--- Code/Code-2025-SSG-A2/Code-2025-SSG-A2/Studys/Navigation/GyeongTabView/GyeongTabviewExampleView.swift
+++ Code/Code-2025-SSG-A2/Code-2025-SSG-A2/Studys/Navigation/GyeongTabView/GyeongTabviewExampleView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct GyeongTabviewExampleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      TabView {
+        Tab("Recieived", systemImage: "tray.and.arrow.down.fill") {
+          ReceivedView()
+        }
+        .badge(2)
+        
+        Tab("Sent", systemImage: "tray.and.arrow.up.fill") {
+          SentView()
+        }
+        
+        Tab("Account", systemImage: "person.crop.circle.fill") {
+          AccountView()
+        }
+        .badge("!")
+      }
     }
+}
+
+struct ReceivedView: View {
+  var body: some View {
+    Text("Received")
+  }
+}
+
+struct SentView: View {
+  var body: some View {
+    Text("Sent")
+  }
+}
+
+struct AccountView: View {
+  var body: some View {
+    Text("Account")
+  }
 }
 
 #Preview {

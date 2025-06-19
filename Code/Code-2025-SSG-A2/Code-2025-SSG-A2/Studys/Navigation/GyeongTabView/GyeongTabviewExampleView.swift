42a291b471e39e6b3147e4d746b3efe67c3b91a7
UnkyungJo <pos10022@unist.ac.kr>
2025-06-19 11:20:01 +0900

    Create GyeongTabviewExampleView.swift

---
+++ Code/Code-2025-SSG-A2/Code-2025-SSG-A2/Studys/Navigation/GyeongTabView/GyeongTabviewExampleView.swift
@@ -0,0 +1,18 @@
+//
+//  GyeongTabviewExampleView.swift
+//  Code-2025-SSG-A2
+//
+//  Created by 조운경 on 6/19/25.
+//
+
+import SwiftUI
+
+struct GyeongTabviewExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GyeongTabviewExampleView()
+}
